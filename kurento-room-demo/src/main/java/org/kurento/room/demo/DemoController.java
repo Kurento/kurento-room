62eb8841f08921e35e45f9a2176f0e28f4b7691f
Radu Tom Vlad <rvlad@naevatec.com>
2015-07-01 12:13:09 +0200

    [room-demo] Throws 404 on room not found
    Fixes the 500 error response on the REST call closeRoom()
    when the room doesn't exist.
    
    Change-Id: Ib67d24a8e1be7837ca5a3a4baecd2af8b40e2517

--- kurento-room-demo/src/main/java/org/kurento/room/demo/DemoController.java
+++ kurento-room-demo/src/main/java/org/kurento/room/demo/DemoController.java
@@ -4,21 +4,37 @@ import org.kurento.room.RoomManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-	private static final Logger log = LoggerFactory.getLogger(DemoController.class);
-	
+	private static final Logger log = LoggerFactory
+			.getLogger(DemoController.class);
+
 	@Autowired
 	private RoomManager roomManager;
 
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public class ResourceNotFoundException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+
+		public ResourceNotFoundException(String msg) {
+			super(msg);
+		}
+	}
+
 	@RequestMapping("/close")
-	public void getAllRooms(@RequestParam("room") String room) {
-		log.warn("Closing the room '{}'", room);
+	public void closeRoom(@RequestParam("room") String room) {
+		log.warn("Trying to close the room '{}'", room);
+		if (!roomManager.getRooms().contains(room)) {
+			log.warn("Unable to close room '{}', not found.", room);
+			throw new ResourceNotFoundException("Room '" + room + "' not found");
+		}
 		roomManager.closeRoom(room);
 	}
 }
