33e2b70afaec3eb1ea2ef30b2f6c40e5006bca64
Radu Tom Vlad <rvlad@naevatec.com>
2016-01-27 15:17:34 +0100

    [room-test] Adds quick reentry test to the same room
    Change-Id: I74563e26e36f96fbebe28e11a5c30714b73e9b1a

---
+++ kurento-room-demo/src/test/java/org/kurento/room/test/demo/OneUserQuickReentryDemoTest.java
@@ -0,0 +1,41 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License (LGPL)
+ * version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.kurento.room.test.demo;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+import org.kurento.room.test.browser.OneUserQuickReentry;
+import org.kurento.test.browser.WebPageType;
+
+/**
+ * @see OneUserQuickReentry
+ * @author Radu Tom Vlad (rvlad@naevatec.com)
+ */
+public class OneUserQuickReentryDemoTest extends OneUserQuickReentry {
+
+  @Override
+  public void setupBrowserTest() throws InterruptedException {
+    webPageType = WebPageType.ROOT;
+    super.setupBrowserTest();
+  }
+
+  @Parameters(name = "{index}: {0}")
+  public static Collection<Object[]> data() {
+    return localChromes(MethodHandles.lookup().lookupClass().getSimpleName(), NUM_USERS,
+        WebPageType.ROOT);
+  }
+}
---
+++ kurento-room-test/src/main/java/org/kurento/room/test/browser/OneUserQuickReentry.java
@@ -0,0 +1,75 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License (LGPL)
+ * version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.kurento.room.test.browser;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.kurento.room.test.RoomFunctionalBrowserTest;
+import org.kurento.test.browser.WebPage;
+
+/**
+ * Room demo integration test (basic version).
+ *
+ * @author Micael Gallego (micael.gallego@gmail.com)
+ * @author Radu Tom Vlad (rvlad@naevatec.com)
+ * @since 5.0.0
+ */
+public class OneUserQuickReentry extends RoomFunctionalBrowserTest<WebPage> {
+
+  public static final int NUM_USERS = 1;
+
+  @Test
+  public void test() throws Exception {
+    ITERATIONS = 5;
+
+    final boolean[] activeUsers = new boolean[NUM_USERS];
+
+    final CountDownLatch[] joinCdl = createCdl(ITERATIONS, NUM_USERS);
+    final CountDownLatch[] leaveCdl = createCdl(ITERATIONS, NUM_USERS);
+
+    iterParallelUsers(NUM_USERS, ITERATIONS, new UserLifecycle() {
+
+      @Override
+      public void run(int numUser, int iteration) throws Exception {
+        String userName = getBrowserKey(numUser);
+        log.info("User '{}' is joining room '{}'", userName, roomName);
+        synchronized (browsersLock) {
+          joinToRoom(numUser, userName, roomName);
+          activeUsers[numUser] = true;
+          verify(activeUsers);
+          joinCdl[iteration].countDown();
+        }
+        log.info("User '{}' joined room '{}'", userName, roomName);
+
+        joinCdl[iteration].await(PLAY_TIME * 5000L, TimeUnit.MILLISECONDS);
+        sleep(0);
+
+        log.info("User '{}' is exiting from room '{}'", userName, roomName);
+        synchronized (browsersLock) {
+          exitFromRoom(numUser, userName);
+          activeUsers[numUser] = false;
+          verify(activeUsers);
+          leaveCdl[iteration].countDown();
+        }
+        log.info("User '{}' exited from room '{}'", userName, roomName);
+        leaveCdl[iteration].await(PLAY_TIME * 5000L, TimeUnit.MILLISECONDS);
+      }
+
+    });
+  }
+
+}
